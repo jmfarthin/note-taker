fd162a1138840b927d5298da6e6ad34d36949ad0
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-04-10 12:41:07 -0400

    changed PORT variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { error } = require('console');
 const generateUniqueId = require('generate-unique-id');
 const app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
